520db7b6fde0034ad11af3e52159eb10e4fbb4b7
Raifulalam <raifulalam0123@gmail.com>
2026-02-01 12:15:33 +0530

    added chnages

--- luckyimpex/src/Pages/HomePage/HeroSlider.jsx
+++ luckyimpex/src/Pages/HomePage/HeroSlider.jsx
@@ -15,9 +15,28 @@ const HeroSlider = () => {
 
     return (
         <section className="hero-slider">
-            <img src={images[currentSlide]} alt={`Slide ${currentSlide + 1}`} className="hero-img" loading="lazy" />
+            <div className="hero-slide">
+                <img
+                    src={images[currentSlide]}
+                    alt={`Lucky Impex Slide ${currentSlide + 1}`}
+                    className="hero-img"
+                    width="1335"
+                    height="890"
+                    loading={currentSlide === 0 ? "eager" : "lazy"}
+                    fetchpriority={currentSlide === 0 ? "high" : "auto"}
+                />
 
+                {/* Text overlay */}
+                {currentSlide === 0 && (  // Show only on first image
+                    <div className="hero-text">
+                        <h1>Premium Electrical & Home Appliances in Nepal</h1>
+                        <p>Wholesale & retail solutions you can trust.</p>
+                    </div>
+                )}
+            </div>
         </section>
+
+
     );
 };
 
