ab21df176c51c5719d7121e9531b2e26957cfb7a
Raifulalam <raifulalam0123@gmail.com>
2026-02-14 15:00:05 +0530

    update redis

--- myStore/config/redis.js
+++ myStore/config/redis.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
 const { createClient } = require("redis");
 
 const redisClient = createClient({
     url: process.env.REDIS_URL,
+    socket: {
+        tls: true,
+        rejectUnauthorized: false,
+    },
 });
 
 redisClient.on("error", (err) => {
@@ -9,8 +14,12 @@ redisClient.on("error", (err) => {
 });
 
 (async () => {
-    await redisClient.connect();
-    console.log("✅ Redis Connected");
+    try {
+        await redisClient.connect();
+        console.log("✅ Redis Connected Successfully");
+    } catch (err) {
+        console.error("❌ Redis Connection Failed:", err);
+    }
 })();
 
 module.exports = redisClient;
